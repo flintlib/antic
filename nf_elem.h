7e90cd48cecf07a94b3543dab86dd15e4ed95c3a
William Hart <goodwillhart@googlemail.com>
2015-04-17 11:00:17 +0100

    Added nf_elem_get_coeff_fmpq.

--- nf_elem.h
+++ nf_elem.h
@@ -376,6 +376,32 @@ void nf_elem_set_fmpq_poly(nf_elem_t a, const fmpq_poly_t pol, const nf_t nf)
 
 /******************************************************************************
 
+    Basic arithmetic
+
+******************************************************************************/
+
+NF_ELEM_INLINE
+void nf_elem_get_coeff_fmpq(fmpq_t a, const nf_elem_t b, 
+                                                        slong i, const nf_t nf)
+{
+   if (nf->flag & NF_LINEAR)
+   {
+      fmpz_set(fmpq_numref(a), LNF_ELEM_NUMREF(b));
+      fmpz_set(fmpq_denref(a), LNF_ELEM_DENREF(b));
+   } else if (nf->flag & NF_QUADRATIC)
+   {
+      const fmpz * const bnum = QNF_ELEM_NUMREF(b);
+      
+      fmpz_set(fmpq_numref(a), bnum + i);
+      fmpz_set(fmpq_denref(a), QNF_ELEM_DENREF(b));
+
+      fmpq_canonicalise(a);
+   } else
+      fmpq_poly_get_coeff_fmpq(a, NF_ELEM(b), i);
+}
+
+/******************************************************************************
+
     Arithmetic
 
 ******************************************************************************/
