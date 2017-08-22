fb237532f6772fc04d6d57cc7cf015e444eb2cf4
William Hart <goodwillhart@googlemail.com>
2017-08-22 09:18:51 +0100

    Fix comments.

--- nf_elem/reduce.c
+++ nf_elem/reduce.c
@@ -15,9 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with FLINT; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-/******************************************************************************
 =============================================================================*/
-
+/******************************************************************************
 
     Copyright (C) 2015 William Hart
 
