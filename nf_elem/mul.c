2b02240838b11c7b04adf2da4cef6b3a6ceaff77
Tommy Hofmann <thofma@gmail.com>
2019-04-02 12:49:13 +0200

    Adjust to recent flint changes

--- nf_elem/mul.c
+++ nf_elem/mul.c
@@ -128,7 +128,7 @@ void _nf_elem_mul_red(nf_elem_t a, const nf_elem_t b,
                _fmpz_vec_set(r, NF_ELEM_NUMREF(a), plen);
 
                _fmpz_poly_divrem(q, NF_ELEM_NUMREF(a), r, plen, 
-                  fmpq_poly_numref(nf->pol), len);
+                  fmpq_poly_numref(nf->pol), len, 0);
 
                _fmpz_vec_clear(r, plen);
                _fmpz_vec_clear(q, plen - len + 1);
