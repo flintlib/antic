78bedf3d88b2c114b85ad8bb7c9c24f8eea7b6ef
Julian Rüth <julian.rueth@fsfe.org>
2021-12-10 21:21:31 -0600

    Detect whether fmpz_gcd3 is available

--- nf_elem/mul.c
+++ nf_elem/mul.c
@@ -56,7 +56,12 @@ void _nf_elem_mul_gaussian(fmpz * anum, fmpz * aden,
    fmpz_mul(aden, bden, cden);
    if (!fmpz_is_one(aden))
    {
+#ifdef HAVE_FMPZ_GCD3
       fmpz_gcd3(t, anum + 0, anum + 1, aden);
+#else
+      fmpz_gcd(t, anum + 0, anum + 1);
+      fmpz_gcd(t, t, aden);
+#endif
       if (!fmpz_is_one(t))
       {
          fmpz_divexact(anum + 0, anum + 0, t);
